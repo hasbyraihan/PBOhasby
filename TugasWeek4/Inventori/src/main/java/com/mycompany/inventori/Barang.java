370586f118db8ed9602fa05a820eccf6f48fce4e
hasbyraihan <117974077+hasbyraihan@users.noreply.github.com>
2023-09-17 22:31:54 +0700

    Add files via upload
    Bismillah

---
+++ TugasWeek4/Inventori/src/main/java/com/mycompany/inventori/Barang.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.inventori;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Barang {
+    String kode_barang;
+    String nama_barang;
+    private int stok;
+
+    public Barang(String kode, String nama, int stk) {
+        kode_barang = kode;
+        nama_barang = nama;
+        stok = stk;
+    }
+    
+    
+    public int getstok() {
+        return stok;
+    }
+    public void tambahStok(int jumlah) {
+        if (jumlah > 0) {
+            stok += jumlah;
+        } else {
+            System.out.println("Jumlah harus positif untuk menambah stok.");
+        }
+    }
+}
+
---
+++ TugasWeek4/KelasDua/src/main/java/com/mycompany/kelasdua/KelasDua.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kelasdua;
+
+/**
+ *
+ * @author ASUS
+ */
+public class KelasDua {
+
+    public static void main(String[] args) {
+        System.out.println(6); 
+        KelasSatu satu = new KelasSatu();
+        KelasSatu dua = new KelasSatu(10);
+    }
+}
