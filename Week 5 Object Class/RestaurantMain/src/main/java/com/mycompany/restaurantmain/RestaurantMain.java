1c017b4bca89a428362db527d5d88b621865d944
hasbyraihan <117974077+hasbyraihan@users.noreply.github.com>
2023-09-25 00:50:34 +0700

    Add files via upload
    Bismillah

---
+++ Week 5 Object Class/RestaurantMain/src/main/java/com/mycompany/restaurantmain/RestaurantMain.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurantmain;
+
+/**
+ *
+ * @author ASUS
+ */
+public class RestaurantMain {
+
+     public static void main(String[] args) {
+    Restaurant menu = new Restaurant();
+    menu.tambahMenuMakanan("Bala-Bala", 1_000, 20);
+    menu.tambahMenuMakanan("Gehu", 1_000, 20);
+    menu.tambahMenuMakanan("Tahu", 1_000, 10);
+    menu.tambahMenuMakanan("Molen", 1_000, 20);
+
+    menu.tampilMenuMakanan();
+
+    menu.pesanMenu("Bala-Bala", 5);
+    menu.pesanMenu("Tahu", 3);
+    menu.pesanMenu("Soto", 2); // Menu tidak ada
+
+    // Menampilkan stok yang tersisa
+    System.out.println("Stok Makanan:");
+    menu.tampilStokMakanan();
+}
+
+}
+
+
